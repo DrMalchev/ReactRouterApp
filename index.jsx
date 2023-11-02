4b9d0600e251db342299e6b414d7f201e2460512
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-02 11:26:26 +0200

    Rounting basics

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
@@ -8,8 +8,17 @@ function App() {
   )
 }
 
+function About() {
+  return (
+    <h1>About page comes here!</h1>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="/about" element={<About />} />
+    </Routes>
   </BrowserRouter>
 );
