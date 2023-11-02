cafc01062e3125eb6a73fd34128a611eff454da2
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-02 15:56:17 +0200

    Home about design ready

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,19 @@ export default function About() {
     return (
         <div>
             <img className='aboutImage' src='./public/images/image54.png'/>
-      <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+            <div className='aboutMain'>
+            <h1 className='abouth1'>Don’t squeeze in a sedan when you could relax in a van.</h1>
+            <p className='abouth2'>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+(Hitch costs extra 😉)
+
+Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+            <div className='aboutLow'>   
+                <p className='aboutLow2'>Your destination is waiting.
+Your van is ready.</p>
+<button className='aboutButton'>Explore our vans</button>
+            </div>
+            </div>
+      
       </div>
     )
   }
