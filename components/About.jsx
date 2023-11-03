fdcce02be42e50adb9239947300c12162b90c029
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-03 10:50:28 +0200

    Refactoring

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <img className='aboutImage' src='./public/images/image54.png'/>
+            <img className='aboutImage' src={'/images/image54.png'}/>
             <div className='aboutMain'>
             <h1 className='abouth1'>Don’t squeeze in a sedan when you could relax in a van.</h1>
             <p className='abouth2'>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
