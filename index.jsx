d59c5def578f9b25fe038d386094bd7425c64ff8
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-06 17:36:42 +0200

    Nesting Header and footer into layout.

--- index.jsx
+++ index.jsx
@@ -4,25 +4,23 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from './components/Home'
 import About from './components/About'
 import Vans from  './components/Vans'
-import Navigation from './components/Navigation'
 import "@fontsource/inter"; // Defaults to weight 400
 import Footer from './components/Footer'
 import VanDetail from './components/VanDetail'
 import "./server"
-
+import Layout from './components/Layout'
 
 function App(){
   return (
-    <BrowserRouter>
-    
-      <Navigation />
+    <BrowserRouter>         
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>
-      <Footer></Footer>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
+      </Routes>      
     </BrowserRouter>
   )
 }
